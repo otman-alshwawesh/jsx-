1bb3de353cd264343e6b8abecc014527a389c243
otman-alshwawesh <alshwaweshalsharidi@gmail.com>
2022-06-20 02:54:49 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './style.css';
+import imageInSrc from "./imageInSrc.webp";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{border:'solid 1px black',maxWidth:('100vw')}}>
+
+<h1 className="title red">Your name here</h1>
+
+<br/>
+
+<img src={imageInSrc} ></img>
+
+<br/>
+
+<img src="/imageInPublic.jpg" ></img>
+
+</div>
+
+<iframe width="853" height="480" src="https://www.youtube.com/embed/2yKT_sxwBCY" title="ريال مدريد يرفع الكأس الغالية" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
   );
 }
